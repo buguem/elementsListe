244d02aeadf14e295c7ac7513c0598ae2d0bf997
buguem <51156546+buguem@users.noreply.github.com>
2019-06-06 08:53:51 -0300

    projet tremblay

---
+++ ElementListes/ElementListesForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ElementListes
+{
+    partial class ElementListeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.suiteDeNbrButton = new System.Windows.Forms.Button();
+            this.lettreAaZButton = new System.Windows.Forms.Button();
+            this.lettreXaCButton = new System.Windows.Forms.Button();
+            this.suiteDiagonaleButton = new System.Windows.Forms.Button();
+            this.fibonnaciButton = new System.Windows.Forms.Button();
+            this.occurenceButton = new System.Windows.Forms.Button();
+            this.inverseButton = new System.Windows.Forms.Button();
+            this.affichageListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // suiteDeNbrButton
+            // 
+            this.suiteDeNbrButton.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.suiteDeNbrButton.Location = new System.Drawing.Point(40, 41);
+            this.suiteDeNbrButton.Name = "suiteDeNbrButton";
+            this.suiteDeNbrButton.Size = new System.Drawing.Size(298, 39);
+            this.suiteDeNbrButton.TabIndex = 0;
+            this.suiteDeNbrButton.Text = "1.Suite de nombres";
+            this.suiteDeNbrButton.UseVisualStyleBackColor = true;
+            this.suiteDeNbrButton.Click += new System.EventHandler(this.suiteDeNbrButton_Click);
+            // 
+            // lettreAaZButton
+            // 
+            this.lettreAaZButton.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lettreAaZButton.Location = new System.Drawing.Point(40, 105);
+            this.lettreAaZButton.Name = "lettreAaZButton";
+            this.lettreAaZButton.Size = new System.Drawing.Size(298, 39);
+            this.lettreAaZButton.TabIndex = 1;
+            this.lettreAaZButton.Text = "2.Lettre A a Z";
+            this.lettreAaZButton.UseVisualStyleBackColor = true;
+            this.lettreAaZButton.Click += new System.EventHandler(this.lettreAaZButton_Click);
+            // 
+            // lettreXaCButton
+            // 
+            this.lettreXaCButton.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lettreXaCButton.Location = new System.Drawing.Point(40, 177);
+            this.lettreXaCButton.Name = "lettreXaCButton";
+            this.lettreXaCButton.Size = new System.Drawing.Size(298, 39);
+            this.lettreXaCButton.TabIndex = 2;
+            this.lettreXaCButton.Text = "3.Lettre X a C";
+            this.lettreXaCButton.UseVisualStyleBackColor = true;
+            this.lettreXaCButton.Click += new System.EventHandler(this.lettreXaCButton_Click);
+            // 
+            // suiteDiagonaleButton
+            // 
+            this.suiteDiagonaleButton.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.suiteDiagonaleButton.Location = new System.Drawing.Point(40, 238);
+            this.suiteDiagonaleButton.Name = "suiteDiagonaleButton";
+            this.suiteDiagonaleButton.Size = new System.Drawing.Size(298, 39);
+            this.suiteDiagonaleButton.TabIndex = 3;
+            this.suiteDiagonaleButton.Text = "4. Suite Diagonale";
+            this.suiteDiagonaleButton.UseVisualStyleBackColor = true;
+            this.suiteDiagonaleButton.Click += new System.EventHandler(this.suiteDiagonaleButton_Click);
+            // 
+            // fibonnaciButton
+            // 
+            this.fibonnaciButton.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fibonnaciButton.Location = new System.Drawing.Point(40, 291);
+            this.fibonnaciButton.Name = "fibonnaciButton";
+            this.fibonnaciButton.Size = new System.Drawing.Size(298, 39);
+            this.fibonnaciButton.TabIndex = 4;
+            this.fibonnaciButton.Text = "5.Fibonnaci";
+            this.fibonnaciButton.UseVisualStyleBackColor = true;
+            this.fibonnaciButton.Click += new System.EventHandler(this.fibonnaciButton_Click);
+            // 
+            // occurenceButton
+            // 
+            this.occurenceButton.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.occurenceButton.Location = new System.Drawing.Point(40, 351);
+            this.occurenceButton.Name = "occurenceButton";
+            this.occurenceButton.Size = new System.Drawing.Size(298, 39);
+            this.occurenceButton.TabIndex = 5;
+            this.occurenceButton.Text = "6.Occurences";
+            this.occurenceButton.UseVisualStyleBackColor = true;
+            this.occurenceButton.Click += new System.EventHandler(this.occurenceButton_Click);
+            // 
+            // inverseButton
+            // 
+            this.inverseButton.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inverseButton.Location = new System.Drawing.Point(40, 410);
+            this.inverseButton.Name = "inverseButton";
+            this.inverseButton.Size = new System.Drawing.Size(298, 39);
+            this.inverseButton.TabIndex = 6;
+            this.inverseButton.Text = "7.Inverse";
+            this.inverseButton.UseVisualStyleBackColor = true;
+            this.inverseButton.Click += new System.EventHandler(this.inverseButton_Click);
+            // 
+            // affichageListBox
+            // 
+            this.affichageListBox.FormattingEnabled = true;
+            this.affichageListBox.ItemHeight = 16;
+            this.affichageListBox.Location = new System.Drawing.Point(379, 48);
+            this.affichageListBox.Name = "affichageListBox";
+            this.affichageListBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.affichageListBox.Size = new System.Drawing.Size(377, 404);
+            this.affichageListBox.TabIndex = 8;
+            // 
+            // ElementListeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 486);
+            this.Controls.Add(this.affichageListBox);
+            this.Controls.Add(this.inverseButton);
+            this.Controls.Add(this.occurenceButton);
+            this.Controls.Add(this.fibonnaciButton);
+            this.Controls.Add(this.suiteDiagonaleButton);
+            this.Controls.Add(this.lettreXaCButton);
+            this.Controls.Add(this.lettreAaZButton);
+            this.Controls.Add(this.suiteDeNbrButton);
+            this.Name = "ElementListeForm";
+            this.Text = "Gerer les elements d\'une liste ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button suiteDeNbrButton;
+        private System.Windows.Forms.Button lettreAaZButton;
+        private System.Windows.Forms.Button lettreXaCButton;
+        private System.Windows.Forms.Button suiteDiagonaleButton;
+        private System.Windows.Forms.Button fibonnaciButton;
+        private System.Windows.Forms.Button occurenceButton;
+        private System.Windows.Forms.Button inverseButton;
+        private System.Windows.Forms.ListBox affichageListBox;
+    }
+}
+
---
+++ ElementListes/ElementListesForm.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ElementListes
+{
+    public partial class ElementListeForm : Form
+    {
+        public ElementListeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void lettreAaZButton_Click(object sender, EventArgs e)
+        {
+            affichageListBox.Items.Clear();
+            Keys keyAscii;
+
+            for (keyAscii = Keys.A; keyAscii<=Keys.Z ; keyAscii++)
+            {
+                affichageListBox.Items.Add(keyAscii);
+            }
+        }
+
+        private void lettreXaCButton_Click(object sender, EventArgs e)
+        {
+            affichageListBox.Items.Clear();
+            int caractereInteger;
+
+            for (caractereInteger = (int)Keys.X + 32; caractereInteger >= (int)Keys.C + 32;
+                caractereInteger--) 
+            {
+                affichageListBox.Items.Add(Convert.ToChar(caractereInteger));
+            }
+        }
+
+        private void suiteDiagonaleButton_Click(object sender, EventArgs e)
+        {
+            affichageListBox.Items.Clear();
+            for (int i = 2; i <= 20 ; i+=2)
+            {
+                affichageListBox.Items.Add(i.ToString().PadLeft(i)); 
+            }
+        }
+
+        private void fibonnaciButton_Click(object sender, EventArgs e)
+        {
+            affichageListBox.Items.Clear();
+
+            int[] fibo = new int[24];
+
+            // affichageListBox.Items.AddRange(new Object[] { 1, 2 });
+            fibo[0] = 0;
+            fibo[1] = 1;
+
+            affichageListBox.Items.Add(fibo[0]);
+            affichageListBox.Items.Add(fibo[1]);
+
+            for (int i = 2; i < 23; i++)
+            {
+               fibo[i] = fibo[i - 1] + fibo[i-2];
+                affichageListBox.Items.Add(fibo[i]);
+            }
+        }
+
+        private void occurenceButton_Click(object sender, EventArgs e)
+        {
+            affichageListBox.Items.Clear();
+            string[] pays = new string[6] { "canada","japonais","Etats-Unis","Maroc","Chine","Bresil" };
+           affichageListBox.Items.AddRange(pays);
+
+            
+
+            for (int i = pays.Length ; i < 6; i++)
+           {
+              
+            }
+           //////for (string s in affichageListBox.Items)
+           //// {
+           ////     if (occurencesCaracteres(s, "a")>=1)
+           ////     {
+           ////         affichageListBox.SelectedItems.Add(s);
+           ////     }
+           //// }
+        }
+        
+        private int occurencesCaracteres(string message, string caractere)
+        {
+            affichageListBox.Items.Clear();
+
+            int occurencesCaractereInt = 0;
+            if (message != null && caractere!=null)
+            {
+                int positionCaractereInt;
+
+                positionCaractereInt = message.IndexOf(caractere, StringComparison.InvariantCultureIgnoreCase);
+
+                while (positionCaractereInt!=1)
+                {
+                    occurencesCaractereInt += 1;
+
+                    positionCaractereInt = message.IndexOf(caractere, positionCaractereInt + 1, StringComparison.InvariantCultureIgnoreCase);
+                }
+            }
+            return 2;
+        }
+
+        private void inverseButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void suiteDeNbrButton_Click(object sender, EventArgs e)
+        {
+
+            for (double nb = -0.75; nb < 2.5; nb += 0.25) 
+            {
+                affichageListBox.Items.Add(nb.ToString("N2").PadLeft(5));
+            }
+        }
+    }
+}
